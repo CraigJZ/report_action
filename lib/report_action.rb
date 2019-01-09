959e50cd2c5171d5f1290079d86cc8d1776f6f6b
Craig Zaccaro <czaccaro@live.com>
2019-01-09 14:20:31 -0500

    Update .gitignore. Introduce core reporting methods.

--- lib/report_action.rb
+++ lib/report_action.rb
@@ -1,6 +1,27 @@
-require "report_action/version"
+require 'report_action/version'
 
 module ReportAction
   class Error < StandardError; end
-  # Your code goes here...
+
+  def initialize_report
+    @report = {}
+  end
+
+  def report_item(target, group, message)
+    @report[target] ||= {}
+    @report[target][group] ||= []
+    @report[target][group] << message
+  end
+
+  def build_report
+    @report_body = ''
+    @report.each do |target, groups|
+      groups.each do |group, messages|
+        @report_body += "<h1>#{group.capitalize}</h1>"
+        messages.uniq.sort.each do |message|
+          @report_body += "<p>#{message}</p>"
+        end
+      end
+    end
+  end
 end
--- test/report_action_test.rb
+++ test/report_action_test.rb
@@ -1,11 +1,46 @@
-require "test_helper"
+require 'test_helper'
 
 class ReportActionTest < Minitest::Test
+  include ReportAction
   def test_that_it_has_a_version_number
     refute_nil ::ReportAction::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+    initialize_report
+  end
+
+  def test_that_it_has_a_report
+    assert @report
+  end
+
+  def test_that_initial_report_is_empty_hash
+    assert_equal @report, {}
+  end
+
+  def test_that_report_is_populated_with_report_items
+    report_item('Test Report', 'Test Section', 'Test Message')
+    report_item('Test Report', 'Test Section', 'Test Message Two')
+    assert_equal @report, 'Test Report' => { 'Test Section' => ['Test Message', 'Test Message Two'] }
+  end
+
+  def test_that_report_structures_multiple_groups
+    report_item('Test Report', 'Test Section', 'Test Message')
+    report_item('Test Report', 'Test Section Two', 'Test Message')
+    assert_equal @report, 'Test Report' => { 'Test Section' => ['Test Message'], 'Test Section Two' => ['Test Message'] }
+  end
+
+  def test_that_report_structures_multiple_targets
+    report_item('Test Report', 'Test Section', 'Test Message')
+    report_item('Test Report Two', 'Test Section', 'Test Message')
+    assert_equal @report, 'Test Report' => { 'Test Section' => ['Test Message'] }, 'Test Report Two' => { 'Test Section' => ['Test Message'] }
+  end
+
+  def test_report_body_is_generated
+    report_item('Test Report', 'Test Section', 'Test Message')
+    report_item('Test Report', 'Test Section', 'Test Message Two')
+    report_item('Test Report', 'Test Section Two', 'Test Message')
+    build_report
+    assert_equal @report_body, '<h1>Test section</h1><p>Test Message</p><p>Test Message Two</p><h1>Test section two</h1><p>Test Message</p>'
   end
 end
