5b10a8a73caa89ae11d2065f63280b5a3c71e522
Craig Zaccaro <czaccaro@live.com>
2019-01-10 16:06:52 -0500

    Improve naming. Add method for purging report_action. Build process reports individually.

--- lib/report_action.rb
+++ lib/report_action.rb
@@ -4,24 +4,27 @@ module ReportAction
   class Error < StandardError; end
 
   def initialize_report
-    @report = {}
+    @report_action = {}
   end
 
-  def report_item(target, group, message)
-    @report[target] ||= {}
-    @report[target][group] ||= []
-    @report[target][group] << message
+  def report_item(process, group, message)
+    @report_action[process] ||= {}
+    @report_action[process][group] ||= []
+    @report_action[process][group] << message
   end
 
-  def build_report
-    @report_body = ''
-    @report.each do |target, groups|
-      groups.each do |group, messages|
-        @report_body += "<h1>#{group.capitalize}</h1>"
-        messages.uniq.sort.each do |message|
-          @report_body += "<p>#{message}</p>"
-        end
+  def build_report(process)
+    @report_action[process].each do |group, messages|
+      @report_body = ''
+      @report_body += "<h1>#{group.capitalize}</h1>"
+      messages.uniq.sort.each do |message|
+        @report_body += "<p>#{message}</p>"
       end
     end
+    @report_action[process] = nil
+  end
+
+  def purge_report
+    @report_action = nil
   end
 end
