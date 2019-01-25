be49731559116b9fb0074ed36430d5165751d742
Danny Collier <dcollie2@gmu.edu>
2019-01-25 12:01:31 -0500

    Move functionality into class within gem namespace.

--- test/report_action_test.rb
+++ test/report_action_test.rb
@@ -7,100 +7,63 @@ class ReportActionTest < Minitest::Test
   end
 
   def setup
-    initialize_report_action
+    @my_report = ReportAction::Report.new
   end
 
-  def test_that_it_has_a_report
-    assert @report_action
+  def test_build_report_is_empty
+    assert_equal @my_report.build_report('test'), ''
   end
 
-  def test_that_initial_report_is_empty_hash
-    assert_equal @report_action, {}
+  def test_report_is_built_for_first_process
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message Two')
+    @my_report.report_item('Test Report Two', 'Test Section Two', 'Test Message')
+    assert_equal @my_report.build_report('Test Report'), '<h1>Test section</h1><p>Test Message</p><p>Test Message Two</p>'
   end
 
-  def test_that_report_is_populated_with_report_items
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    assert_equal @report_action, 'Test Report' => { 'Test Section' => ['Test Message', 'Test Message Two'] }
+  def test_report_is_built_for_second_process
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message Two')
+    @my_report.report_item('Test Report Two', 'Test Section Two', 'Test Message')
+    assert_equal @my_report.build_report('Test Report Two'), '<h1>Test section two</h1><p>Test Message</p>'
   end
 
-  def test_that_report_structures_multiple_groups
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section Two', 'Test Message')
-    assert_equal @report_action, 'Test Report' => { 'Test Section' => ['Test Message'], 'Test Section Two' => ['Test Message'] }
+  def test_report_is_built_for_two_groups_in_a_process
+    @my_report.report_item('Test Report', 'Test Group', 'Test Message')
+    @my_report.report_item('Test Report', 'Test Group Two', 'Test Message Two')
+
+    assert_equal @my_report.build_report('Test Report'), '<h1>Test group</h1><p>Test Message</p><h1>Test group two</h1><p>Test Message Two</p>'
   end
 
-  def test_that_report_structures_multiple_processes
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report Two', 'Test Section', 'Test Message')
-    assert_equal @report_action, 'Test Report' => { 'Test Section' => ['Test Message'] }, 'Test Report Two' => { 'Test Section' => ['Test Message'] }
-  end
-
-  def test_report_body_is_generated_for_first_process
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    report_item('Test Report Two', 'Test Section Two', 'Test Message')
-    build_report('Test Report')
-    assert_equal @report_body, '<h1>Test section</h1><p>Test Message</p><p>Test Message Two</p>'
-  end
-
-  def test_report_body_is_generated_for_second_process
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    report_item('Test Report Two', 'Test Section Two', 'Test Message')
-    build_report('Test Report Two')
-    assert_equal @report_body, '<h1>Test section two</h1><p>Test Message</p>'
-  end
-
-  def test_report_body_is_generated_for_two_groups_in_a_process
-    report_item('Test Report', 'Test Group', 'Test Message')
-    report_item('Test Report', 'Test Group Two', 'Test Message Two')
-    build_report('Test Report')
-    assert_equal @report_body, '<h1>Test group</h1><p>Test Message</p><h1>Test group two</h1><p>Test Message Two</p>'
-  end
-
-  def test_report_body_is_generated_for_two_processes
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    report_item('Test Report Two', 'Test Section Two', 'Test Message')
-    build_report('Test Report')
-    report_one = @report_body
-    build_report('Test Report Two')
-    report_two = @report_body
+  def test_report_is_built_for_two_processes
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message Two')
+    @my_report.report_item('Test Report Two', 'Test Section Two', 'Test Message')
+    report_one = @my_report.build_report('Test Report')
+    report_two = @my_report.build_report('Test Report Two')
     assert_equal report_one, '<h1>Test section</h1><p>Test Message</p><p>Test Message Two</p>'
     assert_equal report_two, '<h1>Test section two</h1><p>Test Message</p>'
   end
 
-  def test_report_action_process_is_set_to_nil_after_report_build
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    report_item('Test Report Two', 'Test Section Two', 'Test Message')
-    build_report('Test Report')
-    assert_nil @report_action['Test Report']
-    assert_equal @report_action, 'Test Report' => nil, 'Test Report Two' => { 'Test Section Two' => ['Test Message'] }
-  end
-
-  def test_report_action_process_remains_for_processes_not_built
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report', 'Test Section', 'Test Message Two')
-    report_item('Test Report Two', 'Test Section Two', 'Test Message')
-    build_report('Test Report')
-    assert_equal @report_action['Test Report Two'], 'Test Section Two' => ['Test Message']
-    assert_equal @report_action, 'Test Report' => nil, 'Test Report Two' => { 'Test Section Two' => ['Test Message'] }
-  end
-
-  def test_that_entire_report_can_be_purged
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report Two', 'Test Section', 'Test Message')
-    assert_equal @report_action, 'Test Report' => { 'Test Section' => ['Test Message'] }, 'Test Report Two' => { 'Test Section' => ['Test Message'] }
-    purge_report
-    assert_nil @report_action
-  end
-
   def test_that_process_list_is_returned
-    report_item('Test Report', 'Test Section', 'Test Message')
-    report_item('Test Report Two', 'Test Section', 'Test Message')
-    process_list = list_report_processes
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    @my_report.report_item('Test Report Two', 'Test Section', 'Test Message')
+    process_list = @my_report.list_report_processes
     assert_equal process_list, ["Test Report", "Test Report Two"]
   end
+
+  def test_has_messages_with_missing_process_returns_false
+    refute @my_report.has_messages?("Missing Process","Missing Group")
+  end
+
+  def test_has_messages_with_missing_group_returns_false
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    refute @my_report.has_messages?("Test Report","Missing Group")
+  end
+
+  def test_has_messages_with_messages_returns_true
+    @my_report.report_item('Test Report', 'Test Section', 'Test Message')
+    assert_equal @my_report.has_messages?("Test Report","Test Message"), false
+  end
+
 end
